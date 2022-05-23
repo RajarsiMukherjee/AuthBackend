e94761646668fa61135652de11f6ea0446643d87
RajarsiMukherjee <96103218+RajarsiMukherjee@users.noreply.github.com>
2022-05-24 00:17:53 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -9,11 +9,12 @@ app.use(express.json());
 
 let port = process.env.PORT || 2344;
 
+const paymentRoutes = require("./controllers/payment");
 
 
 const productApi = require("./controllers/ProductsController");
 
- 
+const passport = require("./configs/google-oauth");
 
 
 app.use(cors());
@@ -34,7 +35,32 @@ app.use("/api/payment/", paymentRoutes);
 
 app.use("/product", productApi);
 
- 
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/auth/google/failure",
+  }),
+  (req, res) => {
+    const { user } = req;
+    console.log("user", user);
+    const token = newToken(user);
+    console.log("user", user);
+    return res.send({ user, token });
+  }
+);
 
 app.listen(port, async (req, res) => {
   try {
